7087613d8f01086e0a13866373d06f2e5d45f2cf
yoyooong <948353466@qq.com>
2023-07-09 14:05:16 +0800

    SARS-CoV-2：optimize the baseline

--- SARS-CoV-2/baseline.py
+++ SARS-CoV-2/baseline.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from rdkit import Chem
 from rdkit import RDLogger
+from rdkit.Chem import AllChem
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle as pkl
 import pandas as pd
 from threading import Thread, Lock
 from pahelix.utils.compound_tools import mol_to_geognn_graph_data_MMFF3d
-from rdkit.Chem import AllChem
 import paddle as pdl
 import paddle.nn as nn
 from pahelix.model_zoo.gem_model import GeoGNNModel
@@ -20,8 +20,10 @@ from paddle import optimizer
 import numpy as np
 from pprint import pprint
 import paddle.nn.functional as F
-from sklearn.metrics import average_precision_score, roc_auc_score, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import average_precision_score, roc_auc_score, accuracy_score, precision_score, recall_score, \
+    f1_score, confusion_matrix
 import warnings
+
 warnings.filterwarnings('ignore')
 import paddle.nn.functional as F
 
@@ -30,7 +32,7 @@ train_df = pd.read_csv('data/train.csv')
 print(f'len of train_df is {len(train_df)}')
 
 # 将smiles转化为rdkit标准smiles
-RDLogger.DisableLog('rdApp.*') # 屏蔽RDKit的warning
+RDLogger.DisableLog('rdApp.*')  # 屏蔽RDKit的warning
 for index, row in train_df.iterrows():
     try:
         mol = Chem.MolFromSmiles(row['SMILES'])
@@ -69,6 +71,8 @@ pkl.dump(smiles_list, open('work/test_smiles_list.pkl', 'wb'))
 
 # 使用分子力场将smiles转化为3d分子图，并保存为smiles_to_graph_dict.pkl文件
 mutex = Lock()  # 互斥锁，防止多个线程同时修改某一文件或某一全局变量，引发未知错误
+
+
 def calculate_3D_structure_(smiles_list):
     n = len(smiles_list)
     global p
@@ -78,10 +82,10 @@ def calculate_3D_structure_(smiles_list):
         if p >= n:
             mutex.release()
             break
-        index = p        # p指针指向的位置为当前线程要处理的smiles
+        index = p  # p指针指向的位置为当前线程要处理的smiles
         smiles = smiles_list[index]
         print(index, ':', round(index / n * 100, 2), '%', smiles)
-        p += 1           # 修改全局变量p
+        p += 1  # 修改全局变量p
         mutex.release()  # 释放锁
         try:
             molecule = AllChem.MolFromSmiles(smiles)
@@ -96,9 +100,10 @@ def calculate_3D_structure_(smiles_list):
             continue
 
         global smiles_to_graph_dict
-        mutex.acquire()   # 获取锁
+        mutex.acquire()  # 获取锁
         smiles_to_graph_dict[smiles] = molecule_graph
-        mutex.release()   # 释放锁
+        mutex.release()  # 释放锁
+
 
 # for mode in ['train', 'test']:
 # # for mode in ['test']:
@@ -123,8 +128,8 @@ def calculate_3D_structure_(smiles_list):
 
 
 # 将smiles、graph、label构建成一个列表，并保存为data_list.pkl文件，该文件为GEM读取的数据文件
-train_smiles_to_graph_dict = pkl.load(open(f'work/train_smiles_to_graph_dict.pkl','rb'))
-test_smiles_to_graph_dict = pkl.load(open(f'work/test_smiles_to_graph_dict.pkl','rb'))
+train_smiles_to_graph_dict = pkl.load(open(f'work/train_smiles_to_graph_dict.pkl', 'rb'))
+test_smiles_to_graph_dict = pkl.load(open(f'work/test_smiles_to_graph_dict.pkl', 'rb'))
 train_data_list = []
 test_data_list = []
 
@@ -154,6 +159,7 @@ for index, row in test_df.iterrows():
 pkl.dump(train_data_list, open('work/train_data_list.pkl', 'wb'))
 pkl.dump(test_data_list, open('work/test_data_list.pkl', 'wb'))
 
+
 class ADMET(nn.Layer):
     def __init__(self):
         super(ADMET, self).__init__()
@@ -175,10 +181,10 @@ class ADMET(nn.Layer):
         node_repr, edge_repr, graph_repr = self.encoder(atom_bond_graph.tensor(), bond_angle_graph.tensor())
         return self.mlp(graph_repr)
 
+
 def collate_fn(data_batch):
     """
-    Dataloader中的数据处理函数
-    该函数输入一个batch的数据, 返回一个batch的(atom_bond_graph, bond_angle_graph, label)
+    Dataloader中的数据处理函数：该函数输入一个batch的数据, 返回一个batch的(atom_bond_graph, bond_angle_graph, label)
     """
     atom_names = ["atomic_num", "formal_charge", "degree", "chiral_tag", "total_numHs", "is_aromatic", "hybridization"]
     bond_names = ["bond_dir", "bond_type", "is_in_ring"]
@@ -194,30 +200,19 @@ def collate_fn(data_batch):
         ab_g = pgl.Graph(
             num_nodes=len(graph[atom_names[0]]),
             edges=graph['edges'],
-            node_feat={name: graph[name].reshape([-1, 1]) for name in atom_names},
-            edge_feat={name: graph[name].reshape([-1, 1]) for name in bond_names + bond_float_names})
+            node_feat={name: graph[name] for name in atom_names},
+            edge_feat={name: graph[name] for name in bond_names + bond_float_names})
         ba_g = pgl.Graph(
             num_nodes=len(graph['edges']),
             edges=graph['BondAngleGraph_edges'],
             node_feat={},
-            edge_feat={name: graph[name].reshape([-1, 1]) for name in bond_angle_float_names})
+            edge_feat={name: graph[name] for name in bond_angle_float_names})
         atom_bond_graph_list.append(ab_g)
         bond_angle_graph_list.append(ba_g)
         label_list.append(data_item['label'])
 
     atom_bond_graph = pgl.Graph.batch(atom_bond_graph_list)
     bond_angle_graph = pgl.Graph.batch(bond_angle_graph_list)
-
-    # TODO: reshape due to pgl limitations on the shape
-    def _flat_shapes(d):
-        """TODO: reshape due to pgl limitations on the shape"""
-        for name in d:
-            d[name] = d[name].reshape([-1])
-
-    _flat_shapes(atom_bond_graph.node_feat)
-    _flat_shapes(atom_bond_graph.edge_feat)
-    _flat_shapes(bond_angle_graph.node_feat)
-    _flat_shapes(bond_angle_graph.edge_feat)
 
     return atom_bond_graph, bond_angle_graph, np.array(label_list, dtype=np.float32)
 
@@ -247,6 +242,7 @@ def get_data_loader(mode, batch_size):
             batch_size=batch_size, num_workers=1, shuffle=False, collate_fn=collate_fn)
         return test_data_loader
 
+
 def trial(model_version, model, batch_size, criterion, scheduler, opt):
     # 创建dataloader
     train_data_loader, valid_data_loader = get_data_loader(mode='train', batch_size=batch_size)
@@ -255,26 +251,37 @@ def trial(model_version, model, batch_size, criterion, scheduler, opt):
     max_bearable_epoch = 50  # 设置早停的轮数为50，若连续50轮内验证集的评价指标没有提升，则停止训练
     current_best_epoch = 0
 
-    train_metric_list = []  # 记录训练过程中各指标的变化情况
-    valid_metric_list = []
+    for epoch in range(1000):  # 设置最多训练1000轮
+        train_label_true = pdl.to_tensor([], dtype=pdl.float32, place=pdl.CUDAPlace(0))
+        train_label_predict = pdl.to_tensor([], dtype=pdl.float32, place=pdl.CUDAPlace(0))
 
-    for epoch in range(800):  # 设置最多训练800轮
         model.train()
         for (atom_bond_graph, bond_angle_graph, label_true_batch) in train_data_loader:
             label_predict_batch = model(atom_bond_graph, bond_angle_graph)
             label_true_batch = pdl.to_tensor(label_true_batch, dtype=pdl.int64, place=pdl.CUDAPlace(0))
+            train_label_true = pdl.concat((train_label_true, label_true_batch.detach()), axis=0)
+            label_predict_batch_softmax = F.softmax(label_predict_batch)
+            train_label_predict = pdl.concat((train_label_predict, label_predict_batch_softmax.detach()), axis=0)
+
             loss = criterion(label_predict_batch, label_true_batch)
             loss.backward()  # 反向传播
             opt.step()  # 更新参数
             opt.clear_grad()
         scheduler.step()  # 更新学习率
 
-        # 评估模型在训练集、验证集的表现
-        metric_train = evaluate(model, train_data_loader)
-        metric_valid = evaluate(model, valid_data_loader)
+        valid_label_true = pdl.to_tensor([], dtype=pdl.float32, place=pdl.CUDAPlace(0))
+        valid_label_predict = pdl.to_tensor([], dtype=pdl.float32, place=pdl.CUDAPlace(0))
+        model.eval()
+        for (atom_bond_graph, bond_angle_graph, label_true_batch) in valid_data_loader:
+            label_predict_batch = model(atom_bond_graph, bond_angle_graph)
+            label_true_batch = pdl.to_tensor(label_true_batch, dtype=pdl.int64, place=pdl.CUDAPlace(0))
+            valid_label_true = pdl.concat((valid_label_true, label_true_batch.detach()), axis=0)
+            label_predict_batch_softmax = F.softmax(label_predict_batch)
+            valid_label_predict = pdl.concat((valid_label_predict, label_predict_batch_softmax.detach()), axis=0)
 
-        train_metric_list.append(metric_train)
-        valid_metric_list.append(metric_valid)
+        # 评估模型在训练集、验证集的表现
+        metric_train = evaluate(train_label_true, train_label_predict)
+        metric_valid = evaluate(valid_label_true, valid_label_predict)
 
         score = round((metric_valid['ap'] + metric_valid['auc']) / 2, 4)
 
@@ -283,30 +290,16 @@ def trial(model_version, model, batch_size, criterion, scheduler, opt):
             current_best_metric = score
             current_best_epoch = epoch
             pdl.save(model.state_dict(), "weight/" + model_version + ".pkl")
-        print("=========================================================")
-        print("Epoch", epoch)
-        pprint(("Train", metric_train))
-        pprint(("Validate", metric_valid))
-        print('current_best_epoch', current_best_epoch, 'current_best_metric', current_best_metric)
+        print('current_epoch', epoch, 'current_best_epoch', current_best_epoch, 'current_best_metric',
+              current_best_metric)
         if epoch > current_best_epoch + max_bearable_epoch:
             break
 
-    return train_metric_list, valid_metric_list
+    return;
 
 
-def evaluate(model, data_loader):
+def evaluate(label_true, label_predict):
     """评估模型"""
-    model.eval()
-    label_true = pdl.to_tensor([], dtype=pdl.float32, place=pdl.CUDAPlace(0))
-    label_predict = pdl.to_tensor([], dtype=pdl.float32, place=pdl.CUDAPlace(0))
-    for (atom_bond_graph, bond_angle_graph, label_true_batch) in data_loader:
-        label_predict_batch = model(atom_bond_graph, bond_angle_graph)
-        label_true_batch = pdl.to_tensor(label_true_batch, dtype=pdl.int64, place=pdl.CUDAPlace(0))
-        label_predict_batch = F.softmax(label_predict_batch)
-
-        label_true = pdl.concat((label_true, label_true_batch.detach()), axis=0)
-        label_predict = pdl.concat((label_predict, label_predict_batch.detach()), axis=0)
-
     y_pred = label_predict[:, 1].cpu().numpy()
     y_true = label_true.cpu().numpy()
 
@@ -324,6 +317,7 @@ def evaluate(model, data_loader):
               'confusion_mat': confusion_mat}
     return metric
 
+
 # 固定随机种子
 # SEED = 1024
 # pdl.seed(SEED)
@@ -331,19 +325,19 @@ def evaluate(model, data_loader):
 # random.seed(SEED)
 
 model = ADMET()
-batch_size = 1024                                                                   # batch size
-criterion = nn.CrossEntropyLoss()                                                   # 损失函数
-scheduler = optimizer.lr.CosineAnnealingDecay(learning_rate=1e-3, T_max=15)         # 余弦退火学习率
-opt = optimizer.Adam(scheduler, parameters=model.parameters(), weight_decay=1e-5)   # 优化器
+batch_size = 512  # batch size
+criterion = nn.CrossEntropyLoss()  # 损失函数
+scheduler = optimizer.lr.CosineAnnealingDecay(learning_rate=1e-3, T_max=15)  # 余弦退火学习率
+opt = optimizer.Adam(scheduler, parameters=model.parameters(), weight_decay=1e-5)  # 优化器
+trial(model_version='1', model=model, batch_size=batch_size, criterion=criterion, scheduler=scheduler, opt=opt)
 
-metric_train_list, metric_valid_list = trial(model_version='1', model=model, batch_size=batch_size, criterion=criterion, scheduler=scheduler, opt=opt)
 
 # 将测试集的预测结果保存为result.csv
 def test(model_version):
     test_data_loader = get_data_loader(mode='test', batch_size=1024)
 
     model = ADMET()
-    model.set_state_dict(pdl.load("weight/" + model_version + ".pkl"))   # 导入训练好的的模型权重
+    model.set_state_dict(pdl.load("weight/" + model_version + ".pkl"))  # 导入训练好的的模型权重
     model.eval()
 
     all_result = []
@@ -356,5 +350,6 @@ def test(model_version):
     df = pd.read_csv('data/test_nolabel.csv')
     df[f'pred'] = all_result
     df.to_csv('result.csv', index=False)
+
 
 test(model_version='1')
